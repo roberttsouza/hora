b11204f06ecb9641785832c45a398021b2cd6d35
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2022-10-17 22:51:35 -0300

    :rocket: script finalizado
    feito a finalização do estilo e codificado a função.
    Realizado também o deploy da aplicação

---
+++ script.js
@@ -0,0 +1,21 @@
+const hora = document.querySelector("#hora")
+const minuto = document.querySelector("#minuto")
+const segundo = document.querySelector("#segundo")
+
+const relogio = setInterval(function time(){
+  const dataHoje = new Date();
+  let horas = dataHoje.getHours();
+  let minutos = dataHoje.getMinutes();
+  let segundos = dataHoje.getSeconds();
+
+  if(horas < 10) horas = "0" + horas;
+  if(minutos < 10) minutos = "0" + minutos;
+  if(segundos < 10) segundos = "0" + segundos;
+
+  hora.textContent = horas;
+  minuto.textContent = minutos;
+  segundo.textContent = segundos;
+
+})
+ 
+ console.log(segundos)
